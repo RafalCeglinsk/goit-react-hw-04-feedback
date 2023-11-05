ce0fefdddb887595945c375401f1104ccbc7969b
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-11-05 18:44:20 +0100

    Refactor
    Carried out code rafactore

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,59 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 
-class FeedBack extends Component {
-  state = {
-    Good: 0,
-    Neutral: 0,
-    Bad: 0,
+const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = type => {
+    if (type === 'Good') {
+      setGood(good + 1);
+    } else if (type === 'Neutral') {
+      setNeutral(neutral + 1);
+    } else if (type === 'Bad') {
+      setBad(bad + 1);
+    }
   };
 
-  handleFeedback = type => {
-    this.setState(prevState => ({
-      [type]: prevState[type] + 1,
-    }));
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
-  countTotalFeedback() {
-    const { Good, Neutral, Bad } = this.state;
-    return Good + Neutral + Bad;
-  }
-  countPercentageFeedback() {
-    const { Good } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    return totalFeedback === 0 ? 0 : Math.ceil((Good / totalFeedback) * 100);
-  }
-  render() {
-    const { Good, Neutral, Bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const percentageFeedback = this.countPercentageFeedback();
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['Good', 'Neutral', 'Bad']}
-            onLeaveFeedback={this.handleFeedback}
+  const countPercentageFeedback = () => {
+    const totalFeedback = countTotalFeedback();
+    return totalFeedback === 0 ? 0 : Math.ceil((good / totalFeedback) * 100);
+  };
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPercentageFeedback()}
           />
-        </Section>
-        <Section title="Statistics">
-          {totalFeedback > 0 ? (
-            <Statistics
-              good={Good}
-              neutral={Neutral}
-              bad={Bad}
-              total={totalFeedback}
-              positivePercentage={percentageFeedback}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </div>
+  );
+};
 
-export default FeedBack;
+export default Feedback;
